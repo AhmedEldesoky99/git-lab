59f1a2c7d09b70dd49a7cfc6a2f163ec2a16f5c6
Ahmed Eldesoky <ahmedeldedoky99@gmail.com>
2023-01-28 15:12:06 +0200

    done

--- server.js
+++ server.js
@@ -13,20 +13,19 @@ app.get("/eldesoky", (req, res) => {
   res.send("Ahmed youssf br");
 });
 
- app.get('/youssef', (req, res) => {
-  res.send("youssef br")
-})
-
+app.get("/youssef Said Galad", (req, res) => {
+  res.send("youssef br");
+});
+app.get("/youssef Said Youssef", (req, res) => {
+  res.send("youssef br");
+});
 app.get("/Zien", (req, res) => {
   res.send("this is response come from Zien branch");
 });
-
 
 // database server
 mongoose
   .connect("mongodb://127.0.0.1:27017/todo_list")
   .then((_) => console.log("database connected successfully"));
-
-
 
 app.listen(port, () => console.log(`server is listening on port: ${port}`));
