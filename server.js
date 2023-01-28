c348c61b8ae15972c8b43e4e896648bba8f323f9
youssef saeed <yossefsaid555@gmail.com>
2023-01-28 14:51:02 +0200

    update port

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 // variables
-
-const port = 3000;
-
+const ports = 3000;
 const app = express();
 
 //middleware
@@ -15,26 +13,13 @@ app.get("/eldesoky", (req, res) => {
   res.send("Ahmed youssf br");
 });
 
-
 app.get("/youssef", (req, res) => {
   res.send("youssf br");
 });
-
- app.get('/youssef', (req, res) => {
-  res.send("youssef br")
-})
-
-app.get("/Zien", (req, res) => {
-  res.send("this is response come from Zien branch");
-});
-
-
 
 // database server
 mongoose
   .connect("mongodb://127.0.0.1:27017/todo_list")
   .then((_) => console.log("database connected successfully"));
 
-
-
-app.listen(port, () => console.log(`server is listening on port: ${port}`));
+app.listen(ports, () => console.log(`server is listening on port: ${ports}`));
