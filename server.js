c8ef35797d3974f028990df7f0ebf37870203bf0
Ahmed Eldesoky <ahmedeldedoky99@gmail.com>
2023-01-28 14:10:23 +0200

    init

---
+++ server.js
@@ -0,0 +1,17 @@
+// import core module
+const express = require("express");
+const mongoose = require("mongoose");
+
+// variables
+const port = 3001;
+const app = express();
+
+//middleware
+app.use(express.json());
+
+// database server
+mongoose
+  .connect("mongodb://127.0.0.1:27017/todo_list")
+  .then((_) => console.log("database connected successfully"));
+
+app.listen(port, () => console.log(`server is listening on port: ${port}`));
