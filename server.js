0eec2ddf8e59d47a5967d63c86e0f64277fd6c0f
Zien Mohammed Abdelmonaem <78083890+zenab12@users.noreply.github.com>
2023-01-28 14:25:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongoose
   .connect("mongodb://127.0.0.1:27017/todo_list")
   .then((_) => console.log("database connected successfully"));
 
-app.get("/Zien", (re1, res) => {
+app.get("/Zien", (req, res) => {
   res.send("this is response come from Zien branch");
 });
 app.listen(port, () => console.log(`server is listening on port: ${port}`));
